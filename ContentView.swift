27a1b7bbd30da10f510dc86b22a92adfc37d1928
joricky91 <jonathanricky91@gmail.com>
2022-04-09 21:28:16 +0700

    change background color, text color

--- ContentView.swift
+++ ContentView.swift
@@ -3,23 +3,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Image("foodIcon")
+                .resizable()
+                .frame(width: 350, height: 190)
+            
             Text("ISF Dictionary")
                 .font(.system(size: 64))
                 .fontWeight(.bold)
+                .foregroundColor(Color("textColor"))
+                .padding(.bottom)
+            
             Text("This is a dictionary made to give information about Indonesia's street food.")
-                .font(.system(size: 24))
+                .font(.system(size: 30))
                 .multilineTextAlignment(.center)
+                .frame(minWidth: 700, idealWidth: 800, maxWidth: 900)
+                .padding(.bottom, 30)
+            
             Button("Start"){
                 print("Button tapped")
             }
             .font(.title)
-                .padding(EdgeInsets(top: 20, leading: 40, bottom: 20, trailing: 40))
-                .background()
+            .foregroundColor(Color.black)
+            .padding(EdgeInsets(top: 20, leading: 90, bottom: 20, trailing: 90))
+            .background(Color("buttonColor"))
+            .cornerRadius(15)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.red)
+        .background(Color("gorenganColor"))
     }
 }

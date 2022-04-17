94a55f2b5fad55d3ad4a3c9b132a85db1174618b
joricky91 <jonathanricky91@gmail.com>
2022-04-17 12:08:05 +0700

    finishing dictionary view, add foodModel for data model

--- ContentView.swift
+++ ContentView.swift
@@ -4,36 +4,40 @@ struct ContentView: View {
     @State var showPage = false
     
     var body: some View {
-        VStack {
-            Image("foodIcon")
-                .resizable()
-                .frame(width: 350, height: 190)
-            
-            Text("ISF Dictionary")
-                .font(.system(size: 64))
-                .fontWeight(.bold)
-                .foregroundColor(Color("textColor"))
-                .padding(.bottom)
-            
-            Text("This is a dictionary made to give information about Indonesia's street food.")
-                .font(.system(size: 30))
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 20)
-                .padding(.bottom, 30)
-            
-            Button("Start"){
-                self.showPage.toggle()
-            }.sheet(isPresented: $showPage){
-                DictionaryView()
+        NavigationView {
+            VStack {
+                Image("foodIcon")
+                    .resizable()
+                    .frame(width: 350, height: 190)
+                
+                Text("ISF Dictionary")
+                    .font(.system(size: 64))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("textColor"))
+                    .padding(.bottom)
+                
+                Text("This is a dictionary made to give information about Indonesia's street food.")
+                    .font(.system(size: 30))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 30)
+                
+                NavigationLink(destination: DictionaryView()) {
+                    Button("Start"){
+                        print("Tapped")
+                    }
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                    .padding(EdgeInsets(top: 20, leading: 90, bottom: 20, trailing: 90))
+                    .background(Color("buttonColor"))
+                    .border(Color("textColor"), width: 6)
+                    .cornerRadius(15)
+                }
+                
             }
-            .font(.title)
-            .foregroundColor(Color.black)
-            .padding(EdgeInsets(top: 20, leading: 90, bottom: 20, trailing: 90))
-            .background(Color("buttonColor"))
-            .border(Color("textColor"), width: 6)
-            .cornerRadius(15)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("gorenganColor"))
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color("gorenganColor"))
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
---
+++ FoodModel.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  WWDC Project
+//
+//  Created by Jonathan Ricky Sandjaja on 16/04/22.
+//
+
+import Foundation
+
+struct FoodModel {
+    var name: String
+    var image: String
+    var description: String
+}
+
+var foodArr: [FoodModel] = [
+    FoodModel(name: "Martabak", image: "martabak", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")
+]
